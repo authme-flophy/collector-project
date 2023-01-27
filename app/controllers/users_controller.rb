9df22b76c755f7b1890916c9c7405d09db4620cc
waikau maina <ericmaina.dev@gmail.com>
2023-01-27 19:44:53 +0300

    fixed user registration to enable it to generate tokesn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
+  skip_before_action :authenticate_request, only: %i[index, create]
 
   # GET /users
   def index
@@ -15,12 +16,13 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    user = User.create!(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.valid?
+      token = jwt_encode(user_id: user.id)
+      render json: { user: user, token: token}, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
