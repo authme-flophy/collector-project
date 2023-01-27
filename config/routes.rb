d8706f0d3d060ea04133becf8c26ba68630600f8
waikau maina <ericmaina.dev@gmail.com>
2023-01-27 19:52:45 +0300

    createed the routes for the login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :clients
-  resources :users
+  resources :users, only: %i[index, show, update, delete]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
 end
