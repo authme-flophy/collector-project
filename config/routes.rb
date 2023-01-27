a684cce5c53908babab4721d6535d2ad1d73cc27
waikau maina <ericmaina.dev@gmail.com>
2023-01-27 19:56:29 +0300

    fixed some errors in routes.rb and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :clients
-  resources :users, only: %i[index, show, update, delete]
+  resources :users, only: %i[index show update delete]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
