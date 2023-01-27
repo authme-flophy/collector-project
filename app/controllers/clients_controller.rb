c4798eb77e9b5e23c66562bbce51a4e7bcabe9c8
waikau maina <ericmaina.dev@gmail.com>
2023-01-27 19:40:19 +0300

    reduced some boiler code in the users and clients controllers

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -46,6 +46,6 @@ class ClientsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def client_params
-      params.require(:client).permit(:first_name, :last_name, :rank, :status, :debt)
+      params.permit(:first_name, :last_name, :rank, :status, :debt)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+      params.permit(:first_name, :last_name, :email, :password)
     end
 end
